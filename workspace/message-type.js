983989783a3c3d3c6949f4f41d33607291754eef
Nalla-Srinivasu <srinivasnalla369@gmail.com>
2026-07-01 19:02:22 +0530

    practice on SystemMessage,HumanMessage methods using in ai model

---
+++ workspace/message-type.js
@@ -0,0 +1,18 @@
+import { ChatOpenAI } from "@langchain/openai";
+import { SystemMessage,HumanMessage } from "langchain";
+import "dotenv/config"
+
+const model = new ChatOpenAI({
+    model:process.env.AI_MODEL,
+    configuration:{baseURL: process.env.AI_ENDPOINT},
+    apiKey: process.env.AI_API_KEY
+});
+
+const message = [
+    new SystemMessage("You are a helpful AI Assistant, How tech of senior engineert explains simply"),
+    new HumanMessage("Explain Quantum computing to a 10 yr old?")
+];
+
+const response = await model.invoke(message);
+console.log(" AI resopnse: \n");
+console.log(response.content);
